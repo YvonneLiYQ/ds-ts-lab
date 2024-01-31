2f707eaa85f52457729ae3fbaba464b7fda1319a
YangQing Li <2217175013@qq.com>
2024-01-31 21:20:02 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,5 +1,5 @@
 import { colleagues, friends } from './01-basics'
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 function older(f: Friend) : string {
     f.age += 1
     return `${f.name} is now ${f.age}` 
@@ -35,6 +35,25 @@ function highestExtension(cs: Colleague[]): Colleague {
     // Add the new colleague to the array
     colleaguesArray.push(newColleague);
   }
-  
+
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+  ): EmailContact[] {
+    const sorted = colleagues.sort(sorter); // Colleague[] inferred
+    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return result 
+  }
+  
+  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+  function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean): Friend[] {
+    // Use the filter method to find friends that meet the criterion specified by the callback
+    return friends.filter(criterion);
+  }
+  console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));  
+  console.log(findFriends(friends, (friend) => friend.age < 35));
